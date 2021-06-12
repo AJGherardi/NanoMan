a99764d1cccb85fdefeca15ef17f27cee8eb5494
Alexander Gherardi <alexander.2003@live.com>
2021-06-11 23:00:00 -0400

    This is just a test

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,48 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <SD.h>
-#include <Wire.h>
-#include "SparkFun_LIS331.h"
+// #include <Wire.h>
+// #include "SparkFun_LIS331.h"
 
-LIS331 xl;
+// LIS331 xl;
 
-const int sdCS = 10; // TODO: Set CS pin for SD card
-
+const int sdCS = 9;
+const int sdCD = 8;
+unsigned long count = 0;
 unsigned long previousTime = 0;
 unsigned long currentTime = 0;
 
 String dataBuffer;
 File dataFile;
-const char filename[] = "datalog.txt";
+const char filename[] = "data.txt";
 
 void setup()
 {
   // Initialize sensor
-  Wire.begin();
-  xl.setI2CAddr(0x19);
-  xl.begin(LIS331::USE_I2C);
-  xl.setPowerMode(LIS331::NORMAL);
-  xl.setFullScale(LIS331::MED_RANGE);
-  xl.setODR(LIS331::DR_100HZ);
+  // Wire.begin();
+  // xl.setI2CAddr(0x19);
+  // xl.begin(LIS331::USE_I2C);
+  // xl.setPowerMode(LIS331::NORMAL);
+  // xl.setFullScale(LIS331::MED_RANGE);
+  // xl.setODR(LIS331::DR_100HZ);
   // TODO: Initialize SD card
-  Serial.begin(38400);
+  // Wait for SD card to be detected
+  Serial.begin(9600);
+
+  pinMode(sdCD, INPUT);
+  pinMode(sdCS, OUTPUT);
+  while (!digitalRead(sdCD))
+    ;
+  delay(250); // 'Debounce insertion'
+
+  if (!SD.begin(sdCS))
+  {
+    while (true)
+      ;
+  }
+  // Initlize data buffer
+  dataBuffer.reserve(1024);
+  // TODO: Remove serial
   // Open file
   dataFile = SD.open(filename, FILE_WRITE);
   if (!dataFile)
@@ -34,8 +51,6 @@ void setup()
     while (1)
       ;
   }
-  // Save template for spreadsheet
-  dataFile.println("millis,accel\n");
   // Set initial time
   previousTime = micros();
 }
@@ -50,18 +65,19 @@ void loop()
   if (timeElapsed >= 10000)
   {
     // Record value
-    int16_t x, y,z ; 
-    xl.readAxes(x, y, z);
-    float xG = xl.convertToG(200, x);
+    int16_t x, y, z;
+    // xl.readAxes(x, y, z);
+    // float xG = xl.convertToG(200, x);
     // Add to buffer
-    dataBuffer += xG;
+
+    // dataBuffer += count;
+    dataBuffer += String(count, DEC);
     dataBuffer += ",";
-    dataBuffer += timeElapsed;
+    dataBuffer += String(timeElapsed, DEC);
     dataBuffer += "\n"; // This creates a new line
     // Mark sample time
-    previousTime = now;
-    Serial.println(dataBuffer);
-    dataBuffer = ""; // TODO:: Remove currently being zeroed to allow prints to serial console
+    previousTime = currentTime;
+    count++;
   }
 
   // Write to file if it is available
@@ -70,6 +86,8 @@ void loop()
   {
     // Write to file
     dataFile.write(dataBuffer.c_str(), chunkSize);
+    dataFile.flush();
+
     // Remove data from buffer
     dataBuffer.remove(0, chunkSize);
   }
