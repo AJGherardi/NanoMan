5a9f146d9459c1a219493557cbb28e4b8067b2e1
Alexander Gherardi <alexander.2003@live.com>
2021-06-16 18:19:50 -0400

    Add three axis

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <SD.h>
-// #include <Wire.h>
-// #include "SparkFun_LIS331.h"
-
-// LIS331 xl;
+#include <Wire.h>
+#include "SparkFun_LIS331.h"
+//
+LIS331 xl;
 
 const int sdCS = 9;
 const int sdCD = 8;
@@ -12,26 +12,26 @@ unsigned long count = 0;
 unsigned long previousTime = 0;
 unsigned long currentTime = 0;
 
-String dataBuffer;
+String dataBuffer = "";
 File dataFile;
 const char filename[] = "data.txt";
 
 void setup()
 {
   // Initialize sensor
-  // Wire.begin();
-  // xl.setI2CAddr(0x19);
-  // xl.begin(LIS331::USE_I2C);
-  // xl.setPowerMode(LIS331::NORMAL);
-  // xl.setFullScale(LIS331::MED_RANGE);
-  // xl.setODR(LIS331::DR_100HZ);
+  Wire.begin();
+  xl.setI2CAddr(0x19);
+  xl.begin(LIS331::USE_I2C);
+  xl.setPowerMode(LIS331::NORMAL);
+  xl.setFullScale(LIS331::MED_RANGE);
+  xl.setODR(LIS331::DR_100HZ);
   // Initialize SD card
   pinMode(sdCD, INPUT);
   pinMode(sdCS, OUTPUT);
   // Wait for SD card to be detected
   while (!digitalRead(sdCD))
     ;
-  delay(250); // 'Debounce insertion'
+  delay(100); // Debounce insertion
 
   if (!SD.begin(sdCS))
   {
@@ -42,17 +42,13 @@ void setup()
   dataBuffer.reserve(1536);
 
   SD.remove(filename);
-  delay(200);
   // Open file
   dataFile = SD.open(filename, FILE_WRITE);
   if (!dataFile)
   {
-    // Serial.print("Error Opening File");
     while (1)
       ;
   }
-  // Leave a second for sd card to be ready
-  delay(1000);
   // Set initial time
   previousTime = micros();
 }
@@ -68,15 +64,20 @@ void loop()
   {
     // Record value
     int16_t x, y, z;
-    // xl.readAxes(x, y, z);
-    // float xG = xl.convertToG(200, x);
+    xl.readAxes(x, y, z);
+    float xG = xl.convertToG(200, x);
+    float yG = xl.convertToG(200, y);
+    float zG = xl.convertToG(200, z);
     // Add to buffer
-    dataBuffer += String(count, DEC);
-    dataBuffer += ",";
     dataBuffer += String(currentTime, DEC);
     dataBuffer += ",";
+    dataBuffer += String(xG, DEC);
+    dataBuffer += ",";
+    dataBuffer += String(yG, DEC);
+    dataBuffer += ",";
+    dataBuffer += String(zG, DEC);
+    dataBuffer += ",";
     dataBuffer += dataBuffer.length();
-    dataBuffer += ",123456789";
     dataBuffer += "\n"; // This creates a new line
     // Mark sample time
     previousTime = currentTime;
